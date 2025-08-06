12cfc9c1a7b62a84cd3cbc1d39fdb4f9a72f264a
DanielHabenicht <daniel-habenicht@outlook.de>
2025-08-06 08:56:32 +0000

    add llm pipe

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FilesetResolver, LlmInference } from '@mediapipe/tasks-genai';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,44 @@ import {MatToolbarModule} from '@angular/material/toolbar';
   styleUrl: './app.scss'
 })
 export class App {
+
+  ngOnInit() {
+    FilesetResolver.forGenAiTasks(
+      // path/to/wasm/root
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-genai@latest/wasm"
+    ).then((genai) => {
+        console.log("loaded");
+      LlmInference.createFromOptions(genai, {
+        baseOptions: {
+          modelAssetPath: '/assets/gemma-2b-it-gpu-int8.bin'
+        },
+        maxTokens: 1000,
+        topK: 40,
+        temperature: 0.8,
+        randomSeed: 101
+      }).then(
+        
+        (llmInference) => {
+          console.log("init");
+          llmInference.generateResponse("Hi who are you?").then(
+            (response) => {
+              console.log(response);
+
+            }
+          );
+
+        }
+      );
+
+      
+
+
+    });
+
+    // llmInference.generateResponse(
+    //   inputPrompt,
+    //   (partialResult, done) => {
+    //         document.getElementById('output').textContent += partialResult;
+    // });
+  }
 }
